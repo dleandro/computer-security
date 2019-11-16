c390968fcd93d452362e98058515c1c5884e1bd9
Joao Barata <A44857@alunos.isel.pt>
2019-11-16 17:19:55 +0000

    Changes to Ex 6

--- serie2-1920/src/ClientJava.java
+++ serie2-1920/src/ClientJava.java
@@ -1,18 +1,45 @@
-import javax.net.ssl.SSLSocket;
-import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManagerFactory;
-import java.security.Key;
-import java.security.KeyStore;
-import java.security.NoSuchAlgorithmException;
+import javax.net.ssl.*;
+import java.io.FileInputStream;
+import java.security.*;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 
 public class ClientJava {
 
+    private static String certificatePath="C:\\Users\\joaob\\Desktop\\ISEL\\QUINTO SEMESTRE\\SI\\computer-security\\serie1-1920\\cert-CA\\CA1.cer";
+    private static String keyPath="C:\\Users\\joaob\\Desktop\\ISEL\\QUINTO SEMESTRE\\SI\\computer-security\\serie2-1920\\serie2-e-anexos\\Alice_1.pfx";
+    private static String url="https://www.secure-server.edu:4433/";
+    private static  String password="changeit";
+
     public static void main(String[] args) throws Exception {
         try {
-            TrustManagerFactory trustManagerFactory=createTrustManagerFactory();
+            Certificate certificate=createCertificate(certificatePath);
+            KeyStore keyStore=createKeyStore();
+            addCertificate(keyStore,"certificate",certificate);
+            TrustManagerFactory trustManagerFactory=createTrustManagerFactory(keyStore);
+            TrustManager trustManagers[] = trustManagerFactory.getTrustManagers();
+
+            KeyStore keyStore2=createKeyStore();
+            associateKey(keyStore2,keyPath,password);
+            KeyManagerFactory keyManagerFactory=KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            keyManagerFactory.init(keyStore2,null);
+            KeyManager[] keyManager=keyManagerFactory.getKeyManagers();
+
+
+            SecureRandom random = new SecureRandom();
+            byte bytes[] = new byte[20];
+            random.nextBytes(bytes);
+
+            SSLContext sslContext=SSLContext.getDefault();
+            sslContext.init(keyManager,trustManagers,random);
+
+
+            /*
             SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
 
-            // socket SSL de cliente
+
+           // socket SSL de cliente
             SSLSocket socket = (SSLSocket) factory.createSocket("www.secure-server.edu", 4433);
 
             // Mostrar certificado do servidor
@@ -20,18 +47,60 @@ public class ClientJava {
 
             // mostrar esquemas criptográficos acordados
             System.out.println(socket.getSession().getCipherSuite());
+            */
         }catch (Exception e) {
             e.printStackTrace();
             }
+    }
+
+    private static void associateKey(KeyStore keystore,String Keypath,String password) {
+        try {
+            FileInputStream fis=new FileInputStream(Keypath);
+            keystore.load(fis,password.toCharArray());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void addCertificate(KeyStore keyStore, String certificate, Certificate certificate1) {
+        try {
+            keyStore.setCertificateEntry(certificate,certificate1);
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Certificate createCertificate(String path) {
+        try {
+            FileInputStream fis=new FileInputStream(path);
+            CertificateFactory factory=CertificateFactory.getInstance("X.509");
+            return factory.generateCertificate(fis);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static KeyStore createKeyStore() {
+        try {
+            KeyStore keyStore=KeyStore.getInstance(KeyStore.getDefaultType());
+            keyStore.load(null);
+            return keyStore;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     private static TrustManagerFactory createTrustManagerFactory(KeyStore ks) {
         try {
             TrustManagerFactory factory=TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
             factory.init(ks);
+            return factory;
         } catch (Exception e) {
             e.printStackTrace();
         }
-
+        return null;
     }
 }
