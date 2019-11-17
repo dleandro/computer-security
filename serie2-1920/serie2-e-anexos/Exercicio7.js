94ba36fa4838b08f8511db26f6c1aa98ee19dd35
dleandro <diogo_leandrao@hotmail.com>
2019-11-17 23:18:35 +0000

    issues being printed and github authentication done

--- serie2-1920/serie2-e-anexos/Exercicio7.js
+++ serie2-1920/serie2-e-anexos/Exercicio7.js
@@ -1,14 +1,15 @@
 const express=require('express')
 
-
 const app = express()
 const PORT=8082;
 const request=require('request')
+const url = require('url')
 
 const 
 GOOGLE_CLIENT_ID='24443093752-l4m9is96jp05qkq1r602tapfb5jq577n.apps.googleusercontent.com',
 GITHUB_CLIENT_ID = '8c58a9745405de014b66',
-GITHUB_CLIENT_SECRET = '57abb1941df3de65119a86df620288f69db2e921'
+GITHUB_CLIENT_SECRET = '57abb1941df3de65119a86df620288f69db2e921',
+GITHUB_USER_EMAIL = 'A44868@alunos.isel.pt'
 
 
 app.get('/login', (req, resp) => {
@@ -20,10 +21,10 @@ app.get('/login', (req, resp) => {
       + 'client_id='+ GOOGLE_CLIENT_ID +'&'
       
       // scope "openid email"
-      + 'scope=openid%20email&'
+      + `scope=openid%20email&`
       
       // parameter state should bind the user's session to a request/response
-      + 'state=some-id-based-on-user-session&'
+      + `state=${GITHUB_USER_EMAIL}&`
       
       // response_type for "authorization code grant"
       + 'response_type=code&'
@@ -36,56 +37,70 @@ app.get('/login', (req, resp) => {
       res.end('Hello Captain,\n navigate to /login to start...')
    })
    
+   app.get('/user/login', (req, res) => {
+      
+      res.redirect(302, url.format({
+         protocol: 'https', 
+         hostname: 'github.com', 
+         pathname: 'login/oauth/authorize', 
+         query: { 
+            client_id: GITHUB_CLIENT_ID, 
+         } 
+      }))
+   })
    
-   app.get('/user/login', function(req, res){
-      let 
-      inDanger = false,
-      access_token
+   app.get('/user/login/granted', (req, res) => {
       
-      
-      request.get({
-         headers: {'user-agent': 'node.js'},
-         url: 'https://github.com/login/oauth/authorize',
-         client_id: GITHUB_CLIENT_ID,
-         redirect_uri: 'http://localhost:8082',
-         login: 'A44857',
-         state: 'random-secret',
-         allow_signup: 'false'
-      }, (err, resp, body) => {
-         if (body.state != 'random-secret') {
-            inDanger = true
-            res.end('its a third party')
+      request.post({
+         url: 'https://github.com/login/oauth/access_token',
+         form: {
+            headers: {
+               'Content-Type': 'application/json'
+            },
+            client_id: GITHUB_CLIENT_ID,
+            client_secret: GITHUB_CLIENT_SECRET,
+            code: req.query.code
+            
          }
+      }, (err, resp, body) => {
+         listIssues(body, res)
       })
-      
-      if (inDanger) {
-         return
-      }
+   })
+
+   app.get('user/task', (req, res) => {
 
       request.post({
-         headers: {'user-agent': 'node.js'},
-         url: `https://github.com/login/oauth/access_token`,
-         client_id: GITHUB_CLIENT_ID,
-         client_secret: GITHUB_CLIENT_SECRET,
-         redirect_uri: 'http://localhost:8082',
-         state: 'random-secret'
-         
+         url: `https://www.googleapis.com/tasks/v1/users/${GITHUB_USER_EMAIL}/lists`,
+         form: {
+               "kind": "tasks#taskList",
+               "id": string,
+               "etag": string,
+               "title": string,
+               "updated": datetime,
+               "selfLink": string
+         }
       }, (err, resp, body) => {
-         access_token = resp.access_token
+
+
       })
+
+
+
+   })
    
+   function listIssues(token, res) {
       request.get({
-         headers: {'user-agent': 'node.js'},
-         Authorization: access_token,
-         url: 'https://api.github.com/user/issues'
+         headers: {
+            'user-agent': 'node.js',
+            Authorization: `token ${token.split('&')[0].split('=')[1]}`
+         },
+         url: 'https://api.github.com/repos/dleandro/si2/issues'
       }, (err, resp, body) => {
-         res.end(body.toString())
+         res.end(body.toString() )
       })
+      
+   }
    
-   })
-   
-   var server = app.listen(PORT, function () {
-      var host = server.address().address
-      var port = server.address().port
-      console.log(`ready captain on port ${PORT}`)
-   })
+   app.listen(PORT, () => 
+   console.log(`ready captain on port ${PORT}`)
+   )
